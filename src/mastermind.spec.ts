62aacb0cb23636fb3618252a2fbaf9a2e47ef6d8
amehmeto <42amehmeto@gmail.com>
2020-03-31 16:47:53 +0200

    :alembic: Adding failing case

--- src/mastermind.spec.ts
+++ src/mastermind.spec.ts
@@ -4,6 +4,7 @@ describe('Mastermind kata', () => {
     it.each(
         [
             [0, 0, ['blue'], ['red']],
+            [1, 0, ['blue'], ['blue']],
         ]
     )('should return %i well-placed and %i misplaced given %o secret and %o guess', (wellplaced: number, misplaced: number, secret: string[], guess:string[] ) => {
         expect(evaluate(secret, guess))
