a0c9c734263f8b079aba71ec6309aef5dd96f4ae
amehmeto <42amehmeto@gmail.com>
2020-04-01 14:17:58 +0200

    :recycle: Final renaming

--- src/mastermind.ts
+++ src/mastermind.ts
@@ -2,21 +2,23 @@ export function evaluate(secret: string[], guess: string[]): GuessResult {
     let guessResult: GuessResult = [0, 0]
     for (let i = 0 ; secret[i] ; i++)
         if (guess.includes(secret[i]))
-            guessResult = incrementGuessResult(guess, secret, i, guessResult)
+            guessResult = countIfWellPlaced(guess, secret, i, guessResult)
     return guessResult
 }
 
-function incrementGuessResult(guess: string[], secret: string[], i: number, guessResult: GuessResult): GuessResult {
-    const wellplaced = 0, misplaced = 1
-    if (isSameIndex(guess, secret, i))
-        guessResult[wellplaced]++
-    else
-        guessResult[misplaced]++
+function countIfWellPlaced(
+    guess: string[],
+    secret: string[],
+    i: number,
+    guessResult: GuessResult
+): GuessResult {
+    const WELLPLACED = 0, MISPLACED = 1;
+    isSameIndex(guess, secret, i) ? guessResult[WELLPLACED]++ : guessResult[MISPLACED]++
     return guessResult
 }
 
 function isSameIndex(guess: string[], secret: string[], i: number) {
-    return guess[i] === secret[i];
+    return guess[i] === secret[i]
 }
 
 type GuessResult = [number, number]
